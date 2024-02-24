47a518ec36586516aec740f6404bc5ab9ce8bead
test <abc@163.com>
2024-02-24 16:06:49 +0800

    Git练习提交

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>第一个PHP程序</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+	<?php echo("欢迎进入PHP的世界！！")
+	?>
+</body>
+</html>
